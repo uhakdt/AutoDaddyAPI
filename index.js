ff1bfce9932abd300513e37da3a6730a7a3dfc52
uhakdt <uhakdt@gmail.com>
2023-05-21 03:54:09 +0100

    Fixed payments and webhooks

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const express = require("express");
 const axios = require("axios");
 const admin = require("firebase-admin");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY_DEV);
+const endpointSecret = process.env.STRIPE_SECRET_WEBHOOK;
 
-const stripe = require("stripe")(
-  "sk_test_51MkoADLJE9t4rWObnQV3ZeAJM7moXR7nDUe4KoaJDkulj219tB9V9l2u0EVu9gCGQUq8l9xBJAeL1IQ00l9hgQyt00b0vki7wT"
-);
-const stripeWebhookSecret = "your_stripe_webhook_secret"; // Replace this with your Stripe webhook signing secret
-
+// EXPRESS SETUP
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  bodyParser.json({
+    verify: (req, _, buf) => {
+      try {
+        JSON.parse(buf.toString());
+        req.rawBody = buf.toString();
+      } catch (e) {
+        console.error(`Invalid JSON: ${buf.toString()}`);
+      }
+    },
+  })(req, res, next);
+});
 
-const bodyParser = require("body-parser"); // If you don't have this already, install with npm install --save body-parser
+app.use(cors({ origin: "http://localhost:3000" }));
 
+// FIREBASE SETUP
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
@@ -22,21 +34,9 @@ admin.initializeApp({
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   }),
 });
-
 const db = admin.firestore();
 
-// const YOUR_DOMAIN = 'https://AutoDaddyAPI.uhakdt.repl.co/api/v1';
-const CLIENT_DOMAIN = "http://localhost:3000";
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
+// ENDPOINTS
 app.post("/api/v1/vehicledata/free/:registrationNumber", async (req, res) => {
   var config = {
     method: "post",
@@ -141,85 +141,90 @@ app.post("/api/v1/vehicledata/full", async (req, res) => {
 
 app.get("/api/v1/chatgpt", async (req, res) => {});
 
-app.post("/api/v1/create-checkout-session", async (req, res) => {
-  var vehicleFreeData = JSON.parse(req.body.vehicleFreeData);
-  var tier = JSON.parse(req.body.tier);
-  var userId = JSON.parse(req.body.userId);
+// app.post("/api/v1/create-checkout-session", async (req, res) => {
+//   var vehicleFreeData = JSON.parse(req.body.vehicleFreeData);
+//   var tier = JSON.parse(req.body.tier);
+//   var userId = JSON.parse(req.body.userId);
 
-  let priceId;
+//   let priceId;
 
-  if (tier.toString() === "basic") {
-    priceId = "price_1N5fRvLJE9t4rWObAZejM2KP";
-  } else if (tier.toString() === "full") {
-    priceId = "price_1N5fSNLJE9t4rWObyuziwRXa";
-  } else {
-    res.status(400).send("Invalid tier");
-    return;
-  }
+//   if (tier.toString() === "basic") {
+//     priceId = "price_1N5fRvLJE9t4rWObAZejM2KP";
+//   } else if (tier.toString() === "full") {
+//     priceId = "price_1N5fSNLJE9t4rWObyuziwRXa";
+//   } else {
+//     res.status(400).send("Invalid tier");
+//     return;
+//   }
 
-  try {
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price: priceId,
-          quantity: 1,
-        },
-      ],
-      mode: "payment",
-      success_url: `${CLIENT_DOMAIN}/payment?success=true`,
-      cancel_url: `${CLIENT_DOMAIN}/payment?canceled=true`,
-      automatic_tax: { enabled: true },
-    });
+//   try {
+//     const session = await stripe.checkout.sessions.create({
+//       line_items: [
+//         {
+//           price: priceId,
+//           quantity: 1,
+//         },
+//       ],
+//       mode: "payment",
+//       success_url: `${CLIENT_DOMAIN}/payment?success=true`,
+//       cancel_url: `${CLIENT_DOMAIN}/payment?canceled=true`,
+//       automatic_tax: { enabled: true },
+//     });
 
-    const docRef = db.collection("orders").doc(session.id);
-    await docRef.set({ sessionId: session.id });
+//     const docRef = db.collection("orders").doc(session.id);
+//     await docRef.set({ sessionId: session.id });
 
-    res.redirect(303, session.url);
-  } catch (error) {
-    console.error("Error creating Stripe session:", error);
-    res.status(500).send("Failed to create Stripe session");
-  }
+//     res.redirect(303, session.url);
+//   } catch (error) {
+//     console.error("Error creating Stripe session:", error);
+//     res.status(500).send("Failed to create Stripe session");
+//   }
+// });
+
+app.post("/api/v1/create-payment-intent", async (req, res) => {
+  const { email } = req.body;
+  console.log(req.body);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 100,
+    currency: "gbp",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+    receipt_email: email,
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
 });
 
-app.post("/api/v1/webhook", bodyParser.json(), (request, response) => {
-  let event = request.body;
-  // Only verify the event if you have an endpoint secret defined.
-  // Otherwise use the basic event deserialized with JSON.parse
-  if (process.env.STRIPE_WEBHOOK_SECRET) {
-    // Get the signature sent by Stripe
-    const signature = request.headers["stripe-signature"];
-    try {
-      event = stripe.webhooks.constructEvent(
-        request.body,
-        signature,
-        process.env.STRIPE_WEBHOOK_SECRET
-      );
-    } catch (err) {
-      console.log(`⚠️  Webhook signature verification failed.`, err.message);
-      return response.sendStatus(400);
-    }
-  }
+app.post("/webhooks", (req, res) => {
+  console.log("Webhook received:", req.rawBody); // Debug Statement 1
+  console.log("Headers:", req.headers); // Debug Statement 2
 
+  const sig = req.headers["stripe-signature"];
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(req.rawBody, sig, endpointSecret);
+  } catch (err) {
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
   // Handle the event
   switch (event.type) {
-    case "payment_intent.succeeded":
-      const paymentIntent = event.data.object;
-      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
-      // Then define and call a method to handle the successful payment intent.
-      // handlePaymentIntentSucceeded(paymentIntent);
+    case "payment_intent.succeeded": {
+      const email = event["data"]["object"]["receipt_email"]; // contains the email that will recive the recipt for the payment (users email usually)
+      console.log(`PaymentIntent was successful for ${email}!`);
       break;
-    case "payment_method.attached":
-      const paymentMethod = event.data.object;
-      // Then define and call a method to handle the successful attachment of a PaymentMethod.
-      // handlePaymentMethodAttached(paymentMethod);
-      break;
+    }
     default:
       // Unexpected event type
-      console.log(`Unhandled event type ${event.type}.`);
+      return res.status(400).end();
   }
 
   // Return a 200 response to acknowledge receipt of the event
-  response.send();
+  res.json({ received: true });
 });
 
 app.get("/", (req, res) => {
