958c1a69f11faedee6aaba2af2f7a6c80507f662
uhakdt <uhakdt@gmail.com>
2023-06-08 12:05:49 +0100

    Create pdf from markdown file and upload to folder for user in firebase storage, added nodemon

--- firebase.js
+++ firebase.js
@@ -6,8 +6,13 @@ admin.initializeApp({
     clientEmail: process.env["FIREBASE_CLIENT_EMAIL"],
     privateKey: process.env["FIREBASE_PRIVATE_KEY"].replace(/\\n/g, "\n"),
   }),
+  storageBucket: "autodaddy.appspot.com",
 });
 
 const db = admin.firestore();
+const storage = admin.storage();
 
-module.exports = db;
+module.exports = {
+  db,
+  storage,
+};
--- functions/fetchAndStore.js
+++ functions/fetchAndStore.js
@@ -1,6 +1,9 @@
 const axios = require("axios");
 const { v4: uuidv4 } = require("uuid");
-const db = require("../firebase");
+const { db, storage } = require("../firebase");
+const fs = require("fs");
+const stream = require("stream");
+const markdownpdf = require("markdown-pdf");
 
 const fetchAndStoreVehicleData = async (
   email,
@@ -9,6 +12,7 @@ const fetchAndStoreVehicleData = async (
   ukvdApiKey
 ) => {
   let packages = ["VehicleAndMotHistory", "VdiCheckFull"];
+  let vehicleRegMark = vehicleFreeData.RegistrationNumber.toString();
 
   const fetchData = async (packageName, vehicleRegMark, ukvdApiKey) => {
     const url = `https://uk1.ukvehicledata.co.uk/api/datapackage/${packageName}?v=2&api_nullitems=1&key_vrm=${vehicleRegMark}&auth_apikey=${ukvdApiKey}`;
@@ -55,11 +59,7 @@ const fetchAndStoreVehicleData = async (
   const userDoc = user.docs[0];
   const uid = userDoc.get("uid");
 
-  const dataMain = await fetchAllData(
-    packages,
-    vehicleFreeData.RegistrationNumber.toString(),
-    ukvdApiKey
-  );
+  const dataMain = await fetchAllData(packages, vehicleRegMark, ukvdApiKey);
 
   const orderId = uuidv4();
 
@@ -76,6 +76,63 @@ const fetchAndStoreVehicleData = async (
     vehicleFreeData: vehicleFreeData,
   });
   console.log("Order successfully written to database");
+
+  try {
+    await createPdfAndUploadToStorage(uid, vehicleRegMark, orderId);
+  } catch (error) {
+    console.error("Error occurred while creating or uploading the PDF:", error);
+  }
+};
+
+const createPdfAndUploadToStorage = async (userId, vehicleRegMark, orderId) => {
+  try {
+    let filename = `${vehicleRegMark}_${orderId}.pdf`;
+
+    // Markdown to PDF
+    let markdown = fs.readFileSync("./markdown-sample.md", "utf8");
+    let yourVariable = "Bismillahir Rahmani Rahim";
+    let markdownText = markdown.replace("An h1 header", yourVariable);
+
+    // Create stream for PDF
+    const readable = new stream.Readable();
+    readable._read = () => {};
+    readable.push(markdownText);
+    readable.push(null);
+
+    // Convert markdown to PDF
+    const pdfStream = readable.pipe(markdownpdf());
+
+    // Create writable stream to Firebase storage
+    const bucket = storage.bucket();
+    const filePath = `user_files/${userId}/${filename}`; // Include the userId in the file path
+    const file = bucket.file(filePath);
+    const writeStream = file.createWriteStream({
+      metadata: {
+        contentType: "application/pdf",
+      },
+    });
+
+    pdfStream.pipe(writeStream);
+
+    await new Promise((resolve, reject) => {
+      writeStream.on("finish", resolve);
+      writeStream.on("error", (error) => {
+        console.error(
+          "Error occurred while writing to Firebase Storage:",
+          error
+        );
+        reject(error);
+      });
+    });
+
+    console.log("PDF successfully uploaded to Firebase Storage");
+  } catch (error) {
+    console.error(
+      "Error occurred while creating the PDF or initiating upload:",
+      error
+    );
+    throw error; // Rethrow the error if you want it to propagate
+  }
 };
 
 module.exports = fetchAndStoreVehicleData;
