95a6d3a5bdaa0de1e48cfa9162bef41d1b00a961
uhakdt <uhakdt@gmail.com>
2023-08-16 21:12:50 +0100

    Fixed sample report

--- src/routes/firebaseRoutes.js
+++ src/routes/firebaseRoutes.js
@@ -7,30 +7,14 @@ const router = express.Router();
 router.post("/download-report", async (req, res) => {
   console.log("ﷺ ﷽");
   try {
-    const { orderId, vehicleRegMark, uid } = req.body;
-
-    const orderSnapshot = await db.collection("orders").doc(orderId).get();
-
-    if (!orderSnapshot.exists) {
-      return res.status(404).send("Order not found");
-    }
-
-    const order = orderSnapshot.data();
-
-    // Check if this order belongs to the authenticated user
-    if (order.uid !== uid) {
-      return res.status(403).send("This order does not belong to you");
-    }
-
-    // Create file path
-    const filePath = `user_files/${uid}/reports/${vehicleRegMark}_${orderId}.pdf`;
-
     // Create signed URL
     const bucket = storage.bucket();
-    const [url] = await bucket.file(filePath).getSignedUrl({
-      action: "read",
-      expires: Date.now() + 1000 * 60 * 60, // 1 hour
-    });
+    const [url] = await bucket
+      .file("AutoDaddy_Sample_Report.pdf")
+      .getSignedUrl({
+        action: "read",
+        expires: Date.now() + 1000 * 60 * 60, // 1 hour
+      });
 
     res.json({ url });
   } catch (error) {
