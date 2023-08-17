fec8d846ed9b6f493955aa4785f9f2dcf3fb83d0
uhakdt <uhakdt@gmail.com>
2023-08-17 03:15:23 +0100

    Added facebook deletion request endpoint

--- src/routes/firebaseRoutes.js
+++ src/routes/firebaseRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { db, storage } from "../firebase.js";
+import crypto from "crypto";
 
 const router = express.Router();
 
@@ -22,5 +23,62 @@ router.post("/download-report", async (req, res) => {
     res.status(500).send("Error getting download URL");
   }
 });
+
+router.post("/data-deletion", async (req, res) => {
+  const signed_request = req.body.signed_request;
+  const data = parse_signed_request(signed_request);
+
+  if (!data) {
+    return res.status(400).send("Invalid signed request.");
+  }
+
+  const user_id = data.user_id;
+
+  try {
+    await admin.auth().deleteUser(user_id);
+
+    const confirmation_code = generateUniqueCode();
+    const status_url = `https://www.autodaddy.co.uk/deletion?id=${confirmation_code}`;
+
+    res.json({
+      url: status_url,
+      confirmation_code: confirmation_code,
+    });
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).send("Error deleting user.");
+  }
+});
+
+function parse_signed_request(signed_request) {
+  const [encoded_sig, payload] = signed_request.split(".");
+  const secret = process.env["FACEBOOK_APP_SECRET"];
+
+  const sig = base64_url_decode(encoded_sig);
+  const data = JSON.parse(base64_url_decode(payload));
+
+  const expected_sig = crypto
+    .createHmac("sha256", secret)
+    .update(payload)
+    .digest("base64");
+
+  if (sig !== expected_sig) {
+    console.error("Bad Signed JSON signature!");
+    return null;
+  }
+
+  return data;
+}
+
+function base64_url_decode(input) {
+  return Buffer.from(
+    input.replace("-", "+").replace("_", "/"),
+    "base64"
+  ).toString("utf-8");
+}
+
+function generateUniqueCode() {
+  return crypto.randomBytes(16).toString("hex");
+}
 
 export default router;
